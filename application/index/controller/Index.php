79a2483410ad961a5a2c3cd3318373b6f802a2cc
cinob <cinob@foxmail.com>
2020-03-04 14:24:38 +0800

    add file upload

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,8 @@ namespace app\index\controller;
 use think\facade\Request;
 use app\common\model\Workerman;
 use think\Controller;
+use Qiniu\Auth;
+use Qiniu\Storage\UploadManager;
 
 class Index extends Controller
 {
@@ -24,5 +26,31 @@ class Index extends Controller
         }
         $ws->sendToSingle($touid, '用户' . $uid . ' : ' .$message);
         return '发送成功';
+    }
+
+    public function getToken()
+    {
+        $accessKey = config('qiniu.accessKey');
+        $secretKey = config('qiniu.secretKey');
+        $bucket = config('qiniu.bucket');
+        $auth = new Auth($accessKey, $secretKey);
+        // 生成上传Token
+        return $auth->uploadToken($bucket);
+    }
+
+    public function qiniuUpload()
+    {
+        $file = Request::file('file');
+        $filePath = $file->getInfo('tmp_name');
+        $key = md5(microtime(true)).'.' . pathinfo($file->getInfo('name'), PATHINFO_EXTENSION);
+        $uploadMgr = new UploadManager;
+        list($ret, $err) = $uploadMgr->putFile($this->getToken(), $key, $filePath);
+        if ($err !== null) {
+            var_dump($err);
+        } else {
+            return json([
+                'key' => config('qiniu.cdn').$ret['key']
+            ]);
+        }
     }
 }
